22f7f2a71510ab110d8a1dca76b8eab58e02979e
Rob Higgins <higginsrob@gmail.com>
2015-09-14 07:35:31 +0000

    removed localstorage, sessionstorage, and cookies.  focus on indexedDB

--- README.md
+++ README.md
@@ -1,101 +1,80 @@
-## storage - AMD module for managing IndexedDB (with Promises), localStorage, sessionStorage, and cookies
+## IDB.js - AMD module for managing IndexedDB with Promises.
 
-storage.js is an AMD module (requireJS) that helps manage browser side storage.  It wraps asyncronous IndexedDB methods into Promises, and simplifies db creation/updating.  It also allows you to use the same api to manage localStorage, sessionStorage, and cookies, making your code interchangable between all three.
+idb.js is an [AMD module](http://requirejs.org/) that helps manage browser side storage.  It wraps asyncronous IndexedDB methods into Promises, simplifying db creation, updating, and error catching.
 
-Included in this repo is Kyle Simpson "npo.js" polyfill (https://github.com/getify/native-promise-only), and is only loaded if there is no global Promise method.
+This module depends on a Global Promise that older browsers might not support, you may want to include a pollyfill like [npo.js](https://github.com/getify/native-promise-only).
 
-## IndexedDB
+
+## Example of use:
 ```javascript
-Storage.db({
-  name: 'the_db_name',
-  version: 1,
-  schema: {
-    "the_table_name": {
-      key: {
-        keyPath: 'theKeyPath'
-      },
-      // optional indexes
-      indexes: {
-        name: {},
-        email: { unique: true }
+
+require([ 'idb' ], function(IDB){
+
+  // create or connect to database, returns a promise
+  IDB({
+    name: 'the_db_name',
+    version: 1,
+    schema: {
+      "the_table_name": {
+        key: {
+          keyPath: 'theKeyPath' // example: 'name', 'id', 'uid'
+        },
+        // define optional index schema
+        indexes: {
+          name: {},
+          email: { unique: true }
+          foo: {},
+          bar: {}
+        }
       }
     }
-  }
-}).then(function (db) {
+  }).then(function(db){
 
-  // add records to the table
-  db.add('the_table_name', [
-    { theKeyPath: "first", value: "A" },
-    { theKeyPath: "second", value: "B" },
-    { theKeyPath: "third", value: "C" }
-  ]).then(function(){
-    // created three records
+    // add records to the table
+    db.add('the_table_name', [
+      { theKeyPath: "first", value: "A" },
+      { theKeyPath: "second", value: "B" },
+      { theKeyPath: "third", value: "C" }
+    ]).then(function(){
+      // created three records
+    });
+
+     // delete records from the table
+    db.delete('the_table_name', [ "first", "second", "third"]).then(function(){
+      // deleted three records (by keyPath)
+    });
+
+    // get a record
+    db.get('the_table_name', "second").then(function(response){
+      console.log(response);
+    });
+
+    // update records in the table (by keyPath)
+    db.update('the_table_name',{ theKeyPath: "first", value: "Apple" }).then({
+      // updated one record
+    });
+
+    // list all items in the table
+    db.query('the_table_name').then(function(list) {
+      console.log(list); // array
+    });
+
+    // count
+    db.count('the_table_name').then(function(count){
+      console.log(count); // number of records in the table
+    });
+
+    // clear table
+    db.clear('the_table_name').then(function(){
+      // deleted all records in the table
+    });
+
+    // close db connection
+    db.close();
+
+  }).catch(function (err) {
+    console.error(err);
   });
 
-   // delete records from the table
-  db.delete('the_table_name', [ "first", "second", "third"]).then(function(){
-    // deleted three records (by keyPath)
-  });
-
-  // get a record
-  db.get('the_table_name', "second").then(function(response){
-    console.log(response);
-  });
-
-  // update records in the table (by keyPath)
-  db.update('the_table_name',{ theKeyPath: "first", value: "Apple" }).then({
-    // updated one record
-  });
-
-  // list all items in the table
-  db.query('the_table_name').then(function(list) {
-    console.log(list); // array
-  });
-
-  // count
-  db.count('the_table_name').then(function(count){
-    console.log(count); // number of records in the table
-  });
-
-  // clear table
-  db.clear('the_table_name').then(function(){
-    // deleted all records in the table
-  });
-
-  // close db connection
-  db.close();
-
-}).catch(function (err) {
-  console.error(err);
 });
-```
-
-## LocalStorage
-```javascript
-Storage.local.clear();                    // clear localStorage
-Storage.local.getItem(key);               // get item in storage
-Storage.local.key(index)                  // get key at index
-Storage.local.keys()                      // array of all keys in storage
-Storage.local.setItem(key, value)         // set item in storage
-Storage.local.removeItem(key)             // remove item from storage
-```
-
-## SessionStorage
-```javascript
-Storage.session.clear();                  // clear localStorage
-Storage.session.getItem(key);             // get item in storage
-Storage.session.key(index)                // get key at index
-Storage.session.keys()                    // array of all keys in storage
-Storage.session.setItem(key, value)       // set item in storage
-Storage.session.removeItem(key)           // remove item from storage
-```
-
-## Cookies
-```javascript
-Storage.cookie.clear();                   // clear local items in cookie
-Storage.cookie.getItem(key);              // get item in cookie
-Storage.cookie.key(index)                 // get key at index
-Storage.cookie.keys()                     // array of all keys in cookie
-Storage.cookie.setItem(key, value)        // set cookie
-Storage.cookie.removeItem(key)            // remove item from cookie
 ```
---
+++ idb.js
@@ -0,0 +1,212 @@
+/*
+  ====================================================
+  idb.js
+  AMD module for using IndexedDB with Promises
+  ====================================================
+  @license Open source under MIT
+  Copyright 2015 Robert Higgins All rights reserved
+  ====================================================
+  global Promise
+  ====================================================
+*/
+
+define(function(){
+
+  var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.oIndexedDB || window.msIndexedDB;
+  var IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
+
+  var DB = function(db){
+
+    var self = this;
+
+    this.closed = false;
+
+    this.add = function(table, records, eachCallback) {
+      if(self.closed) throw 'Database has been closed';
+      return new Promise(function(resolve, reject){
+        var transaction = db.transaction(table, "readwrite" );
+        var store = transaction.objectStore(table);
+        if(Object.prototype.toString.call(records) === "[object Object]"){
+          records = [records];
+        }
+        records.forEach(function(record){
+          var request = store.add(record);
+          request.onsuccess = function(e){
+            if(typeof callback === "function"){
+              eachCallback(e, record);
+            }
+          };
+        });
+        transaction.oncomplete = resolve.bind(self, records);
+        transaction.onerror = reject;
+        transaction.onabort = reject;
+      });
+    };
+
+    this.delete = function(table, keys, eachCallback) {
+      if(self.closed) throw 'Database has been closed';
+      return new Promise(function(resolve, reject){
+        var transaction = db.transaction(table, "readwrite" );
+        var store = transaction.objectStore(table);
+        if(Object.prototype.toString.call(keys) === "[object String]"){
+          keys = [keys];
+        }
+        keys.forEach(function(key){
+          var request = store.delete(key);
+          request.onsuccess = function(e){
+            if(typeof callback === "function"){
+              eachCallback(e, key);
+            }
+          };
+        });
+        transaction.oncomplete = resolve.bind(self,keys);
+        transaction.onerror = reject;
+        transaction.onabort = reject;
+      });
+    };
+
+    this.clear = function (table){
+      if(self.closed) throw new Error('Database has been closed');
+      return new Promise(function(resolve, reject){
+        var transaction = db.transaction(table, "readwrite" );
+        var store = transaction.objectStore(table);
+        store.clear();
+        transaction.oncomplete = resolve.bind(self, true);
+        transaction.onerror = function(){ reject(false) };
+        transaction.onabort = function(){ reject(false) };
+      });
+    };
+
+    this.count = function (table){
+      if(self.closed) throw new Error('Database has been closed');
+      return new Promise(function(resolve, reject){
+        var transaction = db.transaction(table, "readonly" );
+        var store = transaction.objectStore(table);
+        var request = store.count();
+        transaction.oncomplete = resolve.bind(self, request.result);
+        transaction.onerror = reject;
+        transaction.onabort = reject;
+      });
+    };
+
+    this.get = function (table, key, eachCallback){
+      if(self.closed) throw new Error('Database has been closed');
+      return new Promise(function(resolve, reject){
+        var transaction = db.transaction(table, "readwrite" );
+        var store = transaction.objectStore(table);
+        var request = store.get(key);
+        request.onsuccess = function(e){
+          if(typeof eachCallback === "function"){
+            eachCallback(e, request.result);
+          }
+        };
+        transaction.oncomplete = resolve.bind(self, request.result);
+        transaction.onerror = reject;
+        transaction.onabort = reject;
+      });
+    };
+
+    this.update = function(table, records, updatecallback){
+      if(self.closed) throw new Error('Database has been closed');
+      return new Promise(function(resolve, reject){
+        var transaction = db.transaction(table, "readwrite" );
+        var store = transaction.objectStore(table);
+        if(Object.prototype.toString.call(records) === "[object Object]"){
+          records = [records];
+        }
+        records.forEach(function(record){
+          if(Object.prototype.toString.call(record) !== "[object Object]") throw new Error('you must use an object to modify changes');
+          if(!record[store.keyPath]) throw new Error('you must specify the index key in your modify object');
+          var request = store.get(record[store.keyPath]);
+          request.onsuccess = function(e){
+            var result = request.result;
+            Object.keys(record).forEach(function(key){
+              result[key] = record[key];
+            });
+            var requestUpdate = store.put(result);
+            requestUpdate.onsuccess = function(e) {
+               if(typeof updatecallback === "function"){
+                 updatecallback(e, result, record);
+               }
+            };
+            requestUpdate.onerror = reject;
+            requestUpdate.onabort = reject;
+          };
+        });
+        transaction.oncomplete = resolve.bind(self, records);
+        transaction.onerror = reject;
+        transaction.onabort = reject;
+      });
+    };
+
+    this.query = function(table, key, listCallback){
+      if(self.closed) throw new Error('Database has been closed');
+      return new Promise(function(resolve, reject){
+        var transaction = db.transaction(table, "readonly" );
+        var store = transaction.objectStore(table);
+        var list = [];
+        if(Object.prototype.toString.call(key) === "[object Object]"){
+          if(key.lower && key.upper){
+            key = IDBKeyRange.bound(key.lower, key.upper);
+          } else if(key.only){
+            key = IDBKeyRange.only(key.only);
+          } else if(key.lower){
+            key = IDBKeyRange.lowerBound(key.lower);
+          } else if(key.upper){
+            key = IDBKeyRange.upperBound(key.upper);
+          }
+        }
+        store.openCursor(key).onsuccess = function(e){
+          var cursor = e.target.result;
+          if(cursor){
+            list.push(cursor.value);
+            if(typeof listCallback === "function"){
+              listCallback(e, cursor.value);
+            }
+            cursor.continue();
+          }
+        };
+        transaction.oncomplete = resolve.bind(self, list);
+        transaction.onerror = reject;
+        transaction.onabort = reject;
+      });
+    };
+
+    this.close = function() {
+      if(this.closed) throw new Error('Database is already closed');
+      db.close();
+      this.closed = true;
+    };
+
+  };
+
+  return function (options) {
+    return new Promise(function(resolve, reject){
+      if(!indexedDB) throw new Error('IndexedDB not supported in this browser');
+      var request = indexedDB.open(options.name, options.version);
+      request.onsuccess = function(e){
+        resolve(new DB(e.target.result));
+      };
+      request.onupgradeneeded = function(e){
+        var db = e.target.result;
+        Object.keys(options.schema).forEach(function(tableName){
+          var table = options.schema[tableName];
+          var store = db.createObjectStore(tableName, table.key);
+          if(Object.prototype.toString.call(table.indexes) === "[object Object]"){
+            Object.keys(table.indexes).forEach(function(indexKey){
+              try { store.index(indexKey); }
+              catch(e) {
+                store.createIndex(
+                  indexKey,
+                  table.indexes[indexKey].key || indexKey,
+                  Object.keys(table.indexes[indexKey]).length ? table.indexes[indexKey] : { unique: false }
+                );
+              }
+            });
+          }
+        });
+      };
+      request.onerror = reject;
+    });
+  };
+});
